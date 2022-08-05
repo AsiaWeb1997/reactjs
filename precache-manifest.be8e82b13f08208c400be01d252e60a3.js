30c3e7932ae77a59d769f759481e85a908d534e1
AsiaWeb1997 <107118132+AsiaWeb1997@users.noreply.github.com>
2022-08-05 17:30:56 +0530

    hello

---
+++ precache-manifest.be8e82b13f08208c400be01d252e60a3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6233c83ca26f15d03e24",
+    "url": "./static/css/main.a92bf958.chunk.css"
+  },
+  {
+    "revision": "6233c83ca26f15d03e24",
+    "url": "./static/js/main.6233c83c.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "./static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "d0c0943e4cb48632d498",
+    "url": "./static/css/2.547cfa20.chunk.css"
+  },
+  {
+    "revision": "d0c0943e4cb48632d498",
+    "url": "./static/js/2.d0c0943e.chunk.js"
+  },
+  {
+    "revision": "3c4ab64ea93c86f4b92b",
+    "url": "./static/js/runtime~main.3c4ab64e.js"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "./static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "./static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "./static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "e0a9ecfb7de3fcaabda35f198df204e2",
+    "url": "./static/media/check-item.e0a9ecfb.svg"
+  },
+  {
+    "revision": "a70868034353e7879fcf0a4a484725e2",
+    "url": "./static/media/HeroImageHLRMobile.a7086803.png"
+  },
+  {
+    "revision": "b2ae386abc9dd7c44049e8155d2c5ac3",
+    "url": "./static/media/HLRHeroMobile.b2ae386a.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7a71bedaebb5943fab77a310acc984ea",
+    "url": "./index.html"
+  }
+];
