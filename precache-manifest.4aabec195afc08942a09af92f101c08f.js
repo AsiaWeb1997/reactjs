41696c34b225153a965ec903082f98de632166f9
AsiaWeb1997 <107118132+AsiaWeb1997@users.noreply.github.com>
2022-09-23 18:35:52 +0530

    hello

---
+++ precache-manifest.4aabec195afc08942a09af92f101c08f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "b79d59ebf1d191e5bc14",
+    "url": "/static/css/main.34feec36.chunk.css"
+  },
+  {
+    "revision": "b79d59ebf1d191e5bc14",
+    "url": "/static/js/main.b79d59eb.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "59ebbed3ebf0eb38126f",
+    "url": "/static/css/2.be4fe6f3.chunk.css"
+  },
+  {
+    "revision": "59ebbed3ebf0eb38126f",
+    "url": "/static/js/2.59ebbed3.chunk.js"
+  },
+  {
+    "revision": "b032b84938893ba038e5",
+    "url": "/static/js/runtime~main.b032b849.js"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "d4da7de2d340228f5f17fb9bfc08c960",
+    "url": "/static/media/carbon-mobile-banner.d4da7de2.png"
+  },
+  {
+    "revision": "a70868034353e7879fcf0a4a484725e2",
+    "url": "/static/media/HeroImageHLRMobile.a7086803.png"
+  },
+  {
+    "revision": "e0a9ecfb7de3fcaabda35f198df204e2",
+    "url": "/static/media/check-item.e0a9ecfb.svg"
+  },
+  {
+    "revision": "b2ae386abc9dd7c44049e8155d2c5ac3",
+    "url": "/static/media/HLRHeroMobile.b2ae386a.png"
+  },
+  {
+    "revision": "4b3644bcbaa44ba7e62264f440c6835a",
+    "url": "/static/media/hone-lab-black-analysis.4b3644bc.png"
+  },
+  {
+    "revision": "689dd6401f5f22410b35d25fd5b36064",
+    "url": "/static/media/hone-lab-black-banner-mobile.689dd640.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "e321cec06f5e0392cdfe4a62f25d58a5",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b6c9efd5989cf0e6efd6e3ba8c83ea7f.js"
+  "/precache-manifest.4aabec195afc08942a09af92f101c08f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
