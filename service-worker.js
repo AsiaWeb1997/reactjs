898ab97bca4b680a2f44e497972ec09c95114aa0
AsiaWeb1997 <107118132+AsiaWeb1997@users.noreply.github.com>
2022-09-23 19:13:51 +0530

    hello

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4aabec195afc08942a09af92f101c08f.js"
+  "./precache-manifest.08a89435bad8f1b6ff019bb198be7e27.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
